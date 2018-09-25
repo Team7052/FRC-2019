93a2e3766aaf1ddf4847dafb45920bd2607330db
CEWL <you@example.com>
2018-09-24 20:22:53 -0400

    Made first subsystem and command

---
+++ src/org/usfirst/frc/team7052/robot/subsystems/DriveTrain.java
@@ -0,0 +1,25 @@
+package org.usfirst.frc.team7052.robot.subsystems;
+
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import org.usfirst.frc.team7052.robot.RobotMap;
+
+/**
+ *
+ */
+public class DriveTrain extends Subsystem {
+
+    	int frontLeftMotor = RobotMap.frontLeftMotor;
+    	int frontRightMotor = RobotMap.frontRightMotor;
+    	int backLeftMotor = RobotMap.backLeftMotor;
+    	int backRightMotor = RobotMap.backRightMotor;
+    	
+
+    public void initDefaultCommand() {
+    }
+    
+    
+}
+
+
+
